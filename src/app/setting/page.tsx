fef0dba48e5f7f5eea45986447badb8ee7dc54bf
Malik-444 <talhahusnain475@gmail.com>
2024-03-11 14:11:45 +0500

    setting page

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,7 +1,53 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { DataTable } from "@/components/DataTable";
+import PageTitle from "@/components/PageTitle";
+import { cn } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table";
 import React from "react";
 
-type Props = {};
+type Payment = {
+  Category: string;
+  Value: string;
+};
 
-export default function Setting({}: Props) {
-  return <div>Setting</div>;
-}
+export const data: Payment[] = [
+  {
+    Category: "Account",
+    Value: "Profile Settings",
+  },
+  {
+    Category: "Notification",
+    Value: "Email",
+  },
+  {
+    Category: "Language",
+    Value: "English",
+  },
+  {
+    Category: "Logout",
+    Value: "Sign Out",
+  },
+];
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "Category",
+    header: "Category",
+  },
+  {
+    accessorKey: "Value",
+    header: "Value",
+  },
+];
+
+const page = (props: Payment) => {
+  return (
+    <div className="flex flex-col gap-5 w-full">
+      <PageTitle title="Setting" />
+      <DataTable data={data} columns={columns} />
+    </div>
+  );
+};
+
+export default page;
